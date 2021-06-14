10fd3d95759494a4000154a52c2d75faf1c23f46
Corban Thompson <corbant25@gmail.com>
2021-06-14 13:05:55 -0700

    FF-4 FF-5 Setup basic structure

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TimeAuto.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class TimeAuto extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+
+        waitForStart();
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/MecanumDrive.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.drive;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class MecanumDrive {
+
+    public MecanumDrive(HardwareMap hardwareMap) {
+
+    }
+}
